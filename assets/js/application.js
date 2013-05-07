68c89e23be5f5ef77ba3539a2512b37f3e813b34
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-05-06 18:00:38 -0700

    Statically position sidebar if > window.height (for #20)

--- assets/js/application.js
+++ assets/js/application.js
@@ -15,5 +15,22 @@ $(function(){
     })
   }, 100);
 
+  (function(){
+
+    function affixBust() {
+      var needh = $(".bs-docs-sidenav").height() + 30;
+      var h = $(window).height();
+      if (h < needh) {
+        $(".bs-docs-sidenav").addClass("affix-bust");
+      } else {
+        $(".bs-docs-sidenav").removeClass("affix-bust");
+      }
+    };
+
+    $(window).on("resize", affixBust);
+    affixBust();
+
+  })();
+
 });
 
